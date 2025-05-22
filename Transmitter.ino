a3bd8c6ca3fae1898f39ee582ea4278b80dfe706
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2025-05-22 01:10:20 -0500

    Update Transmitter.ino

--- Transmitter.ino
+++ Transmitter.ino
@@ -24,7 +24,7 @@ esp_now_peer_info_t peerInfo;
 // callback when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   //Serial.print("\r\nLast Packet Send Status:\t");
-  //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
   if (status == ESP_NOW_SEND_SUCCESS){
     confirmTx = true;
   } else {
